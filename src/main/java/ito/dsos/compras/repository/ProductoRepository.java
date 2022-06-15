bc311cafab1725cd7512c332efd98490046ef157
Raul-Lopez-Cruz <93017723+Raul-Lopez-Cruz@users.noreply.github.com>
2022-06-15 15:52:37 -0500

    v3

--- src/main/java/ito/dsos/compras/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package ito.dsos.compras.repository;
-
-import ito.dsos.compras.model.ProductoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<ProductoModel, Integer> {
-
-}
